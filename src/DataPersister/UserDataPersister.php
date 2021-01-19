8b1af705618808e66f92021eed096a6c692a1f93
mmozos <martinmozos@gmail.com>
2021-01-19 13:38:54 +0100

    Fixed missing namespace

--- src/DataPersister/UserDataPersister.php
+++ src/DataPersister/UserDataPersister.php
@@ -1,35 +1,30 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace App\DataPersister;
 
 use ApiPlatform\Core\DataPersister\DataPersisterInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Entity\Users;
 
-
 /**
  * Description of UserDataPersister
  *
  * @author daniel
  */
-class UserDataPersister implements DataPersisterInterface 
+class UserDataPersister implements DataPersisterInterface
 {
-	
+
 	private $entityManager;
 	private $userPasswordEncoder;
 
 
 	public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $userPasswordEncoder) 
 	{
-		
+
 		$this->entityManager = $entityManager;
 		$this->userPasswordEncoder = $userPasswordEncoder;
-		
+
 	}
 	
 	public function supports($data) : bool
